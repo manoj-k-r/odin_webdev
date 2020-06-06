8b1b87523d82a4c35c1ef3a9b0fe03d7c9422681
Manoj R <manojkr2608@gmail.com>
2020-06-06 01:14:35 -0700

    Minor tictactoe mods

--- ruby_programs/tictactoe.rb
+++ ruby_programs/tictactoe.rb
@@ -8,9 +8,9 @@ class Board
     end
     def display_board(board)
         puts "#{board[0]} | #{board[1]} | #{board[2]}"
-        puts "---------"
+        puts "--+---+--"
         puts "#{board[3]} | #{board[4]} | #{board[5]}"
-        puts "---------"
+        puts "--+---+--"
         puts "#{board[6]} | #{board[7]} | #{board[8]}"
     end
 
@@ -96,7 +96,6 @@ class Game
 
     def new_game
         @game_board=Board.new()
-        @game_board
     end
 
     def restart
